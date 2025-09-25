b39678743e92af9486a643eda79ea5f0458d23ca
amonteserinb64 <amonteserinb64@alumnes.santjosepobrer.es>
2025-09-25 18:33:42 +0200

    Push only cpp files

--- test.cpp
+++ test.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-#include <math.h>
+#include <stdio.h>
+#include <cmath>
+
 using namespace std;
 
 int main(){
-    cout << 2 * 5 << "\n\n";
+    int x;
+    cout << "Introduce tu primer número: ";
+    cin >> x;
+
+    int y;
+    cout << "Introduce tu segundo número: ";
+    cin >> y;
+
+    cout << "Quieres sumar (1), restar(2), multiplicar (3) o dividir (4) estos números? ";
+    int z;
+    cin >> z;
+
+    if (z = 1) {
+        cout << "El resultado es: " << x + y << endl;
+    } else if (z == 2) {
+        cout << "El resultado es: " << x - y << endl;
+    } else if (z == 3) {
+        cout << "El resultado es: " << x * y << endl;
+    } else if (z == 4) {
+        if (y != 0) {
+            cout << "El resultado es: " << x / y << endl;
+        } else {
+            cout << "Error: División por cero no está permitida." << endl;
+        }
+    } else {
+        cout << "Operación no válida." << endl;
+    } 
     cout << "Soy peruano";
     return 0;
 }
